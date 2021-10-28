8de991e439fb54869a271065c6d9ecc4fe0d2c5d
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-10-27 22:42:22 -0300

    feat: measure times sending data to the server

--- practica_3/src/main/java/pdytr/cinco/BlankService.java
+++ practica_3/src/main/java/pdytr/cinco/BlankService.java
@@ -1,14 +1,21 @@
 package pdytr.cinco;
 
 import io.grpc.stub.StreamObserver;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Empty;
 
+import pdytr.cinco.BlankServiceOuterClass.Content;
 import pdytr.cinco.BlankServiceGrpc.BlankServiceImplBase;
 
 public class BlankService extends BlankServiceImplBase {
 
     public void blank(Empty request, StreamObserver<Empty> responseObserver) {
         responseObserver.onNext(Empty.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    public void full(Content request, StreamObserver<Content> responseObserver) {
+        responseObserver.onNext(Content.newBuilder().setData(ByteString.copyFrom(new byte[8])).build());
         responseObserver.onCompleted();
     }
 }
--- practica_3/src/main/java/pdytr/cinco/Client.java
+++ practica_3/src/main/java/pdytr/cinco/Client.java
@@ -4,17 +4,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
+import java.util.Arrays;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import com.google.protobuf.Empty;
+import pdytr.cinco.BlankServiceOuterClass.Content;
 
 import pdytr.cinco.BlankServiceGrpc.BlankServiceBlockingStub;
 import static pdytr.cinco.BlankServiceGrpc.newBlockingStub;
 
 public class Client {
 
-    private static final String database = "src" + File.separator
+    private static final int ITERATIONS = 100000;
+
+    private static final String DATABASE = "src" + File.separator
                                          + "main" + File.separator
                                          + "java" + File.separator
                                          + "pdytr" + File.separator
@@ -28,27 +33,46 @@ public class Client {
             .build();
 
         try {
-            // create stream writer
-            File file = new File(Client.database, "times.csv");
-            FileOutputStream stream = new FileOutputStream(file);
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(stream));
-
             // create the stub
             BlankServiceBlockingStub stub = newBlockingStub(channel);
 
-            // craete the read request
+            // create buffered writer
+            BufferedWriter emptyBw = createStreamWriter("times_con_mensaje_vacio.csv");
+            BufferedWriter fullBw = createStreamWriter("times.csv");
+
+            // craete the read requests
             Empty empty = Empty.newBuilder().build();
+            Content full = Content.newBuilder().setData(createData(1000)).build();
 
             long start, end;
-            for (int i = 0; i < 100000; i++) {
+
+            /*
+             * Measure times when calling the server with an
+             * empty request and expecting an empty response.
+            */
+            for (int i = 0; i < ITERATIONS; i++) {
                 start = System.nanoTime();
                 stub.blank(empty);
                 end = System.nanoTime();
 
-                bw.write(Double.toString((end - start) / 1000000.0));
-                bw.newLine();
+                emptyBw.write(Double.toString((end - start) / 1000000.0));
+                emptyBw.newLine();
             }
-            bw.close();
+            emptyBw.close();
+
+            /*
+             * Now do the same sending a 1000 bytes long request,
+             * expecting an 8 bytes long response.
+            */
+            for (int i = 0; i < ITERATIONS; i++) {
+                start = System.nanoTime();
+                stub.full(full);
+                end = System.nanoTime();
+
+                fullBw.write(Double.toString((end - start) / 1000000.0));
+                fullBw.newLine();
+            }
+            fullBw.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -56,5 +80,15 @@ public class Client {
         } finally {
             channel.shutdownNow();
         }
+    }
+
+    private static ByteString createData(int size) {
+        byte[] data = new byte[size];
+        Arrays.fill(data, (byte)'$');
+        return ByteString.copyFrom(data);
+    }
+
+    private static BufferedWriter createStreamWriter(String filename) throws Exception {
+        return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(DATABASE, filename))));
     }
 }
